e99b9fb12323021660039001462bb60922bdb249
Nathan Stensvad <68618647+NathanStensvad@users.noreply.github.com>
2020-08-19 17:42:07 -0700

    Add style
    I styled my app to make it look nice.

--- script.js
+++ script.js
@@ -11,18 +11,6 @@ function idCleanUp(idName) {
   return idName;
 }
 
-function downloadImageButton() {
-  $('#js-download-showcase').remove();
-  $('#showcase-buttons').append(`<a href="#" class="button" id="js-download-showcase" download="Amiibo_ShowCase">Download Showcase</button>`);
-
-  var button = document.getElementById('js-download-showcase');
-
-  $('#js-download-showcase').on('click',function(e) {
-    console.log("downloading");
-    var dataURL = canvas.toDataURL('image/png');
-    button.href = dataURL;
-  });
-}
 //Button functions for the main amiibos navigation page
 function navigationButtonFunctions() {
   
@@ -47,11 +35,39 @@ function navigationButtonFunctions() {
   });
 
   //Top button
+  $('#js-select-all').on('click',function(e) {
+    e.preventDefault();
+    $('#category').addClass('hidden');
+    $('#showcase').addClass('hidden');
+    $('#results').removeClass('hidden');
+    $('#results').addClass('results-flex');
+
+    if($('.js-amiibos').hasClass('selected')) {
+      if (window.confirm("Deselect all?")) {
+        $('button.js-amiibos').each(function() {
+        $(this).removeClass('selected');
+        });
+      }
+    }
+    else {
+      $('button.js-amiibos').each(function() {
+        $(this).addClass('selected');
+      });
+    }
+    
+    $('button.js-amiibos').each(function() {
+      $(this).removeClass('hidden');
+    });
+  });
+
+  //Top button
   $('#js-showcase').on('click',function(e) {
     e.preventDefault();
     $('#results').addClass('hidden');
+    $('#results').removeClass('results-flex');
     $('#category').addClass('hidden');
-    $('#options').addClass('hidden');
+    $('#options').addClass('hidden'); //why doesn't options work here? I had to make another container to get options to disappear.
+    $("#options-hide").addClass("hidden"); //dummy class cause idk how
     $('#showcase').removeClass('hidden');
     showCase();
   });
@@ -62,6 +78,7 @@ function navigationButtonFunctions() {
     const buttonValue = $(this).val();
     $('#category').addClass('hidden');
     $('#results').removeClass('hidden');
+    $('#results').addClass('results-flex');
     $('button.js-amiibos').each(function() {
       $(this).addClass('hidden');
     });
@@ -80,18 +97,29 @@ function navigationButtonFunctions() {
 
 //Buttons for the showcase screen
 function showcaseButtons() {
-  $('#js-image-convert').on('click',function() {
-    downloadImageButton();
-    convertToImage();
-  });
   $('#js-go-back').on('click',function() {
     $('#options').removeClass('hidden');
     $('#showcase').addClass('hidden');
     $('#results').removeClass('hidden');
+    $('#results').addClass('results-flex');
+    $("#options-hide").removeClass("hidden");
   });
 
   $('#js-reroll').on('click',function() {
     showCase();
+  });
+}
+
+function downloadImageButton() {
+  $('#js-download-showcase').remove();
+  $('#download-button').append(`<a href="#" class="button" id="js-download-showcase" download="Amiibo_ShowCase"><img id="download" src="images/download.png" alt="Download"></a>`);
+
+  var button = document.getElementById('js-download-showcase');
+
+  $('#js-download-showcase').on('click',function(e) {
+    console.log("downloading");
+    var dataURL = canvas.toDataURL('image/png');
+    button.href = dataURL;
   });
 }
 
@@ -126,39 +154,58 @@ function compare(a, b) {
   return comparison;
 }
 
+
 //Take every selected amiibo and showcase it
 function showCase() {
   //I wanted to find the src from the img of the child of this but I couldn't figure out how
   $('#showcase').empty();
   showCaseArray = [];
   $('.selected').each(function(index, element) {
-    showCaseArray.push({image: $(element).val(), randomNumberY: Math.random(), randomNumberX: Math.random()});
+    showCaseArray.push({image: $(element).children()[0].src, randomNumberY: Math.random(), randomNumberX: Math.random()});
   });
 
-  console.log(showCaseArray);
   showCaseArray.sort(compare);
-console.log(showCaseArray);
   
-  $('#showcase').append(`<img src="images/showcase.jpg" class="showcase" id="img1"/>`);
+  $('#showcase').append(`<img src="images/showcase.jpg" class="showcase hidden" id="img1"/>`);
+
   for(let i=0;i<showCaseArray.length;i++) {
     $('#showcase').append(`
-  <img class="show amiiboImg" src="${showCaseArray[i].image}" id="img${i+2}" crossorigin="anonymous">
+  <img class="show amiiboImg hidden" src="${showCaseArray[i].image}" id="img${i+2}" crossorigin="anonymous">
   `);
-  $(`img#img${i+2}`).css({'top': 430+(showCaseArray[i].randomNumberY*120), 'left': 80+(showCaseArray[i].randomNumberX*1040)});
+
+  $(`img#img${i+2}`).css({'top': 530+(showCaseArray[i].randomNumberY*120), 'left': 80+(showCaseArray[i].randomNumberX*1040)});
   }
 
   $('#showcase').append(`
-  <p id="showcase-buttons">
-    <button type="button" id="js-reroll">Reroll Amiibo Placements</button>
-    <button type="button" id="js-go-back">Go Back</button>
-    <button type="button" id="js-image-convert">Convert to Image</button>
+  <p id="showcase-buttons" class="group">
+    <button type="button" id="js-go-back" class="item">Go Back</button>
+    <button type="button" id="js-reroll" class="item">Reroll Amiibo Placements</button>
+  </p>
+  <p id="download-button">
   </p>
   <canvas id="canvas"></canvas>
   `);
+  
+  //Copied this from https://stackoverflow.com/questions/11071314/javascript-execute-after-all-images-have-loaded to make my canvas load properly
+  var imgs = document.images,
+  len = imgs.length,
+  counter = 0;
 
-  showcaseButtons();
-  
-  
+  [].forEach.call( imgs, function( img ) {
+    if(img.complete)
+      incrementCounter();
+    else
+      img.addEventListener( 'load', incrementCounter, false );
+} );
+  function incrementCounter() {
+    counter++;
+    if ( counter === len ) {
+      downloadImageButton();
+      convertToImage();
+      showcaseButtons();
+    }
+}
+
 }
 
 //Check to see if the game series has an amiibo before putting a button down.
@@ -169,23 +216,26 @@ function checkForAmiibos(amiiboName) {
 
 //buttons for sorting amiibos
 function displayGameSeriesButtons(responseJson) {
-  console.log(responseJson);
+  //console.log(responseJson);
 
   $('#category').empty();
+  $('#category').append(`<div id="category-group" class="group">`);
   if(checkForAmiibos(responseJson.amiibo[0].name)) {
-    $('#category').append(`<button class="js-gameseries-buttons" value="${idCleanUp(responseJson.amiibo[0].name)}" type="button" ">${responseJson.amiibo[0].name}</button>`);
+    $('#category-group').append(`<button class="js-gameseries-buttons item" value="${idCleanUp(responseJson.amiibo[0].name)}" type="button" ">${responseJson.amiibo[0].name}</button>`);
   }
   for(let i=1; i<responseJson.amiibo.length;i++) {
     //Filter out multiple entries from original array
     if(responseJson.amiibo[i-1].name !== responseJson.amiibo[i].name) {
       //Make sure there are amiibos for the game series button
       if(checkForAmiibos(responseJson.amiibo[i].name)) {
-        $('#category').append(`<button class="js-gameseries-buttons" value="${idCleanUp(responseJson.amiibo[i].name)}" type="button">${responseJson.amiibo[i].name}</button>`);
+        $('#category-group').append(`<button class="js-gameseries-buttons item" value="${idCleanUp(responseJson.amiibo[i].name)}" type="button">${responseJson.amiibo[i].name}</button>`);
       }
     }
   }
   //Extra category for other entries.
-  $('#category').append(`<button class="js-gameseries-buttons" value="Other" type="button">Other</button>`);
+  //$('#category-group').append(`<button class="js-gameseries-buttons item" value="Other" type="button">Other</button>`);
+
+  $('#category').append(`</div>`);
   //The navigation buttons, game series buttons, and amiibo buttons have all loaded up to this point.
   navigationButtonFunctions();
 }
@@ -194,7 +244,7 @@ function displayGameSeriesButtons(responseJson) {
 function getGameSeries() {
   const url = searchURL + "gameseries";
 
-  console.log(url);
+  //console.log(url);
   fetch(url)
     .then(response => {
       if (response.ok) {
@@ -210,13 +260,13 @@ function getGameSeries() {
 
 //Display amiibo buttons
 function displayAmiibos(responseJson) {
-  console.log(responseJson);
+  //console.log(responseJson);
 
   $('#results').empty();
 
   for(let i=0; i<responseJson.amiibo.length;i++) {
     $('#results').append(`
-      <button type="button" class="js-amiibos" id="${responseJson.amiibo[i].gameSeries}" value="${responseJson.amiibo[i].image}">
+      <button type="button" class="js-amiibos" id="${responseJson.amiibo[i].gameSeries}">
         <img class="amiiboImg" src="${responseJson.amiibo[i].image}" alt="${responseJson.amiibo[i].name}">
       </button>`
     );}
@@ -240,7 +290,7 @@ function getAPI() {
   //const url = searchURL + "amiibo";
   const url = searchURL + "amiibo/?type=figure";
 
-  console.log(url);
+  //console.log(url);
   fetch(url)
     .then(response => {
       if (response.ok) {
@@ -264,6 +314,7 @@ function homePage() {
   $('#js-browse').on('click', function(e) {
     e.preventDefault();
     $('#home').addClass('hidden');
+    $("html").css("background-image", "none");
     renderAmiibos();
   });
 }
